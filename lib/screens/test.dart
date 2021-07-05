d63ee3dc273390f31f8cc0bfd1024d6de577253f
Harsh Patel <harsh.p3@ahduni.edu.in>
2021-07-05 16:20:01 +0530

    signup status routing

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -3,6 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:laughie_app/screens/record_screen.dart';
 import 'package:laughie_app/screens/signIn.dart';
+import 'package:laughie_app/screens/signUpMedicalHistory.dart';
+import 'package:laughie_app/screens/signUpPersonalDetails.dart';
+import 'package:laughie_app/screens/signUpPrescription.dart';
 
 final usersRef = FirebaseFirestore.instance.collection('users');
 
@@ -14,8 +17,23 @@ class Test extends StatelessWidget {
     return StreamBuilder(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, userSnapshot) {
+        var status;
         if (userSnapshot.hasData) {
-          return RecordScreen();
+          usersRef
+              .doc(FirebaseAuth.instance.currentUser.uid)
+              .get()
+              .then((snapshot) {
+            status = (snapshot.data()['signup_status']);
+          });
+          if (status == 0) {
+            return SignUpPersonalDetails();
+          } else if (status == 1) {
+            return SignUpMedicalHistory();
+          } else if (status == 2) {
+            return SignUpPrescription();
+          } else {
+            return RecordScreen();
+          }
         }
         return SignIn();
       },
